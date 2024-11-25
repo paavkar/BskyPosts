4d98d3e78d47fd5f231a555d4465351616488d30
Paavo Karppinen <paavo_karppinen@outlook.com>
2024-11-25 17:07:35 +0200

    Add logic to check post embed type

--- app.py
+++ app.py
@@ -33,15 +33,18 @@ def get_posts():
                         f.post.repost_count, f.post.record.created_at)
             user_posts.append(post)
         else:
-            post_images = []
-            for image in f.post.embed.images:
-                post_images.append(BskyImage(image.alt, image.fullsize,
+            if f.post.embed.py_type == "app.bsky.embed.video#view":
+                continue
+            if f.post.embed.py_type == "app.bsky.embed.images#view":
+                post_images = []
+                for image in f.post.embed.images:
+                    post_images.append(BskyImage(image.alt, image.fullsize,
                                              image.aspect_ratio.width,
                                              image.aspect_ratio.height))
-            post = BskyPost(f.post.uri, f.post.record.text, f.post.author.avatar, f.post.author.handle, f.post.author.display_name,
+                post = BskyPost(f.post.uri, f.post.record.text, f.post.author.avatar, f.post.author.handle, f.post.author.display_name,
                             f.post.like_count, f.post.reply_count, f.post.quote_count,
                             f.post.repost_count, f.post.record.created_at, post_images)
-            user_posts.append(post)
+                user_posts.append(post)
 
     return json.dumps(user_posts, cls=PostEncoder)
 
